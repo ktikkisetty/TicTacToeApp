b206198f6ee29d6e22a706d570da171e4e5d7f14
ktikkise <ktikkisetty@gmail.com>
2022-07-05 16:58:46 -0700

    Grid methods started

--- src/model/Grid.java
+++ src/model/Grid.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Grid {
-    private List<Integer> grid; //
+    private List<Integer> grid; //Actual Grid. 9 spaces possible
+    //Grid Layout
+    // 0    1     2
+    // 3    4     5
+    // 6    7     8
     private List<Integer> available; //contains the positions that can be  (possible values are 0 - 8)
     private static int EMPTY = 0;
     private static int X = 1;
-    private static int O = 0;
+    private static int O = 2;
 
     public Grid() {
         grid = new ArrayList<>();
@@ -27,9 +31,18 @@ public class Grid {
     //TODO: Plan all functions needed (with documentation)
     //need size, empty?, get for each, contains for available...
 
-    //REQUIRES:
-    //MODIFIES:
-    //EFFECTS:
+    //EFFECTS: returns number of spaces that can be filled
+    public int getNumberOfAvailableSpaces() {
+        return available.size();
+    }
 
+    //EFFECTS: Makes sure that space is available to be played
+    public boolean isSpaceAvailable(int spaceNumber) {
+        return available.contains(spaceNumber);
+    }
 
+    //EFFECTS: Checks for one possible game end state
+    public boolean isGridEmpty() {
+        return false;
+    }
 }
